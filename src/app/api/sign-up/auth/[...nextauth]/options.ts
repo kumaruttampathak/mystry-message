eb3f4cead38bfeb501b9828891ee821bc802f108
kumaruttampathak <kumaruttampathak10@gmail.com>
2026-06-24 00:01:37 +0530

    add authentication options and user registration route with email verification

---
+++ src/app/api/sign-up/auth/[...nextauth]/options.ts
@@ -0,0 +1,74 @@
+import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcryptjs"
+import dbConnect from "@/lib/dbConnect"
+import UserModel from "@/model/User"
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: { label: "email", type: "text"},
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials:any): Promise<any>{
+                await dbConnect()
+                try {
+                    const user = await UserModel.findOne({
+                        $or: [
+                            {
+                                email: credentials.identifier
+                            }, 
+                            {
+                                username: credentials.identifier
+                            }
+                        ]
+                    })
+                    if(!user){
+                        throw new Error("No user found with this email")
+                    }
+                    if(!user.isVerified){
+                        throw new Error("Please verify your account first")
+                    }
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+                    if(isPasswordCorrect){
+                        return user
+                    } else {
+                        throw new Error("Incorrect password")
+                    }
+                } catch (err: any) {
+                    throw new Error(err)
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async session({session, token}){
+            if(token){ // feeding the data so that we don't to make databse query to get user
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified;
+                session.user.isAcceptingMessages = token.isAcceptingMessages;
+                session.user.username = token.username;
+            }
+            return session
+        },
+        async jwt({token, user}){
+            if(user){ // we can retrive user from user, session as well as token
+                token._id = user._id?.toString()
+                token.isVerified = user.isVerified;
+                token.isAcceptingMessage = user.isAcceptingMessages;
+                token.username = user.username
+            }
+            return token
+        },
+    },
+    pages: {
+        signIn: '/sigh-in'
+    },
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET
+}
