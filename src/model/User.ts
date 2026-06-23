b81c6ab4dcbf044c53e90f3a2da748ae31ef5517
kumaruttampathak <kumaruttampathak10@gmail.com>
2026-06-23 23:52:32 +0530

    add User and Message schemas for user management

---
+++ src/model/User.ts
@@ -0,0 +1,71 @@
+import mongoose, { Schema, Document, Mongoose } from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  password: string;
+  email: string;
+  verifyCode: string;
+  isVerified: boolean;
+  verifyCodeExpiry: Date;
+  isAcceptingMessage: boolean;
+  message: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "VerifyCode is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  message: [MessageSchema],
+});
+
+// there is a catch while exporting this schema in next js due to this will run at the edge, they don't have idea about weather this application is running first time or it is boot up earlier. so we have to take care while exporting.
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel
